01109ee81e08e1f5c0882c9d9d5b302770f4555b
Liu Suyu <liusy708@gmail.com>
2021-11-21 19:42:29 +0800

    必做作业：整合你上次作业的 httpclient/okhttp

--- 02nio/nio01/src/main/java/java0/nio01/netty/HttpHandler.java
+++ 02nio/nio01/src/main/java/java0/nio01/netty/HttpHandler.java
@@ -9,6 +9,15 @@ import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpUtil;
 import io.netty.util.ReferenceCountUtil;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.concurrent.FutureCallback;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.util.EntityUtils;
 
 import static io.netty.handler.codec.http.HttpHeaderNames.CONNECTION;
 import static io.netty.handler.codec.http.HttpHeaderValues.KEEP_ALIVE;
@@ -17,7 +26,7 @@ import static io.netty.handler.codec.http.HttpResponseStatus.OK;
 import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
 public class HttpHandler extends ChannelInboundHandlerAdapter {
-    
+
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) {
         ctx.flush();
@@ -51,6 +60,13 @@ public class HttpHandler extends ChannelInboundHandlerAdapter {
 //            httpGet ...  http://localhost:8801
 //            返回的响应，"hello,nio";
 //            value = reponse....
+            String url = "http://localhost:8801";
+            final HttpGet httpGet = new HttpGet(url);
+
+            CloseableHttpClient httpclient = HttpClients.createDefault();
+            CloseableHttpResponse httpResponse = httpclient.execute(httpGet);
+            HttpEntity entity = httpResponse.getEntity();
+            value = EntityUtils.toString(entity, "UTF-8");
 
             response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(value.getBytes("UTF-8")));
             response.headers().set("Content-Type", "application/json");
