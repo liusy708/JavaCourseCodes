3360e4b4066c5bcea17d5dfe3f90fec5ad667124
Liu Suyu <liusy708@gmail.com>
2021-11-21 22:26:46 +0800

    选做作业：使用netty实现后端http访问

--- 02nio/nio02/src/main/java/io/github/kimmking/gateway/outbound/netty4/NettyHttpClientOutboundHandler.java
+++ 02nio/nio02/src/main/java/io/github/kimmking/gateway/outbound/netty4/NettyHttpClientOutboundHandler.java
@@ -1,22 +1,51 @@
 package io.github.kimmking.gateway.outbound.netty4;
 
+import static io.netty.handler.codec.http.HttpHeaderNames.*;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.*;
 
-public class NettyHttpClientOutboundHandler  extends ChannelInboundHandlerAdapter {
-    
+public class NettyHttpClientOutboundHandler extends ChannelInboundHandlerAdapter {
+
+    private HttpRequest request;
+
     @Override
     public void channelActive(ChannelHandlerContext ctx)
             throws Exception {
-        
-        
+
+
     }
-    
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg)
             throws Exception {
-        
-        
-        
+
+        if (msg instanceof HttpRequest) {
+            request = (HttpRequest) msg;
+
+            String uri = request.uri();
+            System.out.println("Uri:" + uri);
+        }
+        if (msg instanceof HttpContent) {
+            HttpContent content = (HttpContent) msg;
+            ByteBuf buf = content.content();
+            System.out.println(buf.toString(io.netty.util.CharsetUtil.UTF_8));
+            buf.release();
+
+            FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(msg.toString().getBytes("UTF-8")));
+            response.headers().set(CONTENT_TYPE, "text/plain");
+            response.headers().set(CONTENT_LENGTH,
+                    response.content().readableBytes());
+            response.headers().set(CONNECTION, HttpHeaderValues.KEEP_ALIVE);
+
+            ctx.write(response);
+            ctx.flush();
+        }
+
     }
 }
